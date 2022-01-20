2b8604f3c405723c8b46c2d3e57b02594c9067d3
Lars Heidkämper <lars.heidkaemper@gmail.com>
2022-01-20 22:25:25 +0100

    basic refactoring

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Add anchor functionality for Reactor in Craft CMS 3
 1. Run Composer in your project folder:
 
 ```
-composer require utakka/redactor-anchors
+composer require heidkaemper/redactor-anchors
 ```
 
 2. Open your Craft Control Panel, navigate to Plugins and click 'Install' for Redactor Anchors.
--- src/RedactorAnchors.php
+++ src/RedactorAnchors.php
@@ -1,31 +1,30 @@
 <?php
 
-namespace utakka\redactoranchors;
+namespace heidkaemper\redactoranchors;
 
 use Craft;
-use craft\base\Plugin;
-use craft\redactor\events\RegisterPluginPathsEvent;
-use craft\redactor\Field;
 use yii\base\Event;
+use craft\base\Plugin;
+use craft\redactor\Field;
+use craft\redactor\events\RegisterPluginPathsEvent;
 
+class RedactorAnchors extends Plugin
+{
+    public static $plugin;
 
-class RedactorAnchors extends Plugin {
+    public function init()
+    {
+        parent::init();
 
-  public static $plugin;
+        self::$plugin = $this;
 
-  public function init() {
-
-    parent::init();
-    self::$plugin = $this;
-
-    if( !Craft::$app->request->isConsoleRequest && Craft::$app->request->isCpRequest ) {
-      Event::on(Field::class, Field::EVENT_REGISTER_PLUGIN_PATHS, [$this, 'RegisterPluginPath']);
+        if  (! Craft::$app->request->isConsoleRequest && Craft::$app->request->isCpRequest) {
+            Event::on(Field::class, Field::EVENT_REGISTER_PLUGIN_PATHS, [$this, 'RegisterPluginPath']);
+        }
     }
-      
-  }
 
-  public function RegisterPluginPath(RegisterPluginPathsEvent $event) {
-    $event->paths[] = \dirname(__DIR__) . '/src/resources/';
-  }
-  
+    public function RegisterPluginPath(RegisterPluginPathsEvent $event)
+    {
+        $event->paths[] = \dirname(__DIR__) . '/src/resources/';
+    }
 }
